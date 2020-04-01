b4548f61fe0d6779e307c6364b21e295f9f8b979
Behzad-Rabiei <behzad.rabiei.77@gmail.com>
2020-04-02 00:34:32 +0430

    middleware added to store

--- App/config/store.js
+++ App/config/store.js
@@ -1,5 +1,11 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
+import logger from 'redux-logger';
 import rootReducer from '../reducers/index';
 
-const store = createStore(rootReducer);
+const middleware = [];
+if (process.env.NODE_ENV === 'development') {
+  middleware.push(logger);
+}
+const store = createStore(rootReducer, applyMiddleware(...middleware));
+
 export default store;
