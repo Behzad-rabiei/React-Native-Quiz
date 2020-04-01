0c7dda7ac1c6af271c9dfd7a3eef639a53002c95
Behzad-Rabiei <behzad.rabiei.77@gmail.com>
2020-04-01 20:59:24 +0430

    reducer Updated

--- App/reducers/index.js
+++ App/reducers/index.js
@@ -1,9 +1,25 @@
+import {SET_TOTALCOUNT, CORRECTCOUNT_PLUS} from '../actions/actionsType';
 const initialState = {
   correctCount: 0,
   totalCount: 0,
   activeQuestionIndex: 0,
 };
 
-const rootReducer = (state = initialState, action) => {};
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_TOTALCOUNT: {
+      return {
+        ...state,
+        totalCount: action.value,
+      };
+    }
+    case CORRECTCOUNT_PLUS: {
+      return {
+        ...state,
+        correctCount: state.correctCount + 1,
+      };
+    }
+  }
+};
 
 export default rootReducer;
