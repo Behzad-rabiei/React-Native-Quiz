3c4b65c2ce33a065086a121561b9e35770c98777
Behzad-Rabiei <behzad.rabiei.77@gmail.com>
2020-04-01 17:25:48 +0430

    Enable LogBox

--- index.js
+++ index.js
@@ -5,5 +5,5 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-
+require('react-native').unstable_enableLogBox();
 AppRegistry.registerComponent(appName, () => App);
