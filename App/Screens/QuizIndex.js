3688f1c5c492b808eca196c6d7e5ddf322681eaa
Behzad-Rabiei <behzad.rabiei.77@gmail.com>
2020-04-05 19:37:19 +0430

    QuizIndex screen created

---
+++ App/Screens/QuizIndex.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Button} from 'react-native';
+import {Container} from '../Components/Container';
+
+const QuizIndex = () => (
+  <Container>
+    <Button title="go to quiz page" />
+  </Container>
+);
+
+export default QuizIndex;
