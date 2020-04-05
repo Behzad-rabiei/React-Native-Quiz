988614b3c2a15fbf70f223646424e07d75ff15d7
Behzad-Rabiei <behzad.rabiei.77@gmail.com>
2020-04-05 19:53:44 +0430

    Setup navigation for QuizIndex screen

--- App/Screens/QuizIndex.js
+++ App/Screens/QuizIndex.js
@@ -2,9 +2,12 @@ import React from 'react';
 import {Button} from 'react-native';
 import {Container} from '../Components/Container';
 
-const QuizIndex = () => (
+const QuizIndex = ({navigation}) => (
   <Container QuizIndex>
-    <Button title="go to quiz page" />
+    <Button
+      title="go to quiz page"
+      onPress={() => navigation.navigate('Quiz')}
+    />
   </Container>
 );
 
--- App/config/routes.js
+++ App/config/routes.js
@@ -2,13 +2,19 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import Quiz from '../Screens/Quiz';
+import QuizIndex from '../Screens/QuizIndex';
 
 const MainStack = createStackNavigator();
 
 const Navigator = () => (
   <NavigationContainer>
-    <MainStack.Navigator>
+    <MainStack.Navigator initialRouteName="QuizIndex">
       <MainStack.Screen name="Quiz" component={Quiz} />
+      <MainStack.Screen
+        name="QuizIndex"
+        component={QuizIndex}
+        options={{title: 'Quizzes'}}
+      />
     </MainStack.Navigator>
   </NavigationContainer>
 );
