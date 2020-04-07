1441a61d3fb8ef582a52bba8ff1e2e097601e4bf
Behzad-Rabiei <behzad.rabiei.77@gmail.com>
2020-04-07 11:58:35 +0430

    Props for SummeryText component

--- App/Components/SummeryText/SummeryText.js
+++ App/Components/SummeryText/SummeryText.js
@@ -3,22 +3,22 @@ import {View, Text} from 'react-native';
 import * as Animatable from 'react-native-animatable';
 import styles from './styles';
 
-const SummeryText = () => {
+const SummeryText = ({category, totalCount, correctCount}) => {
   return (
     <View style={styles.container}>
       <Animatable.View animation="zoomIn">
         <Text style={styles.title}>Quiz Finished</Text>
         <Text style={styles.other}>
           Category:
-          <Text style={styles.result}> computers</Text>
+          <Text style={styles.result}>{category}</Text>
         </Text>
         <Text style={styles.other}>
           Questions:
-          <Text style={styles.result}> 20</Text>
+          <Text style={styles.result}>{totalCount}</Text>
         </Text>
         <Text style={styles.other}>
           Correct Answers:
-          <Text style={styles.result}> 15</Text>
+          <Text style={styles.result}>{correctCount}</Text>
         </Text>
         <Text style={styles.other}>
           Time:
--- App/Screens/Summery.js
+++ App/Screens/Summery.js
@@ -1,15 +1,29 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 import {Container} from '../Components/Container';
 import {SummeryText} from '../Components/SummeryText';
 
 class Summery extends Component {
   render() {
+    const {category, totalCount, correctCount} = this.props;
     return (
-      <Container Summery>
-        <SummeryText />
+      <Container Summery backgroundColor={category}>
+        <SummeryText
+          category={category}
+          totalCount={totalCount}
+          correctCount={correctCount}
+        />
       </Container>
     );
   }
 }
 
-export default Summery;
+const mapStateToProps = (state) => {
+  const {totalCount, correctCount, category} = state;
+  return {
+    totalCount,
+    correctCount,
+    category,
+  };
+};
+export default connect(mapStateToProps)(Summery);
