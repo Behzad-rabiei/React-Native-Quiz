b5b80b9feb559e6259c364618f225ef4b88e6fbb
Behzad-Rabiei <behzad.rabiei.77@gmail.com>
2020-04-07 11:51:22 +0430

    SummeryText component animated

--- App/Components/SummeryText/SummeryText.js
+++ App/Components/SummeryText/SummeryText.js
@@ -1,27 +1,30 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import * as Animatable from 'react-native-animatable';
 import styles from './styles';
 
 const SummeryText = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Quiz Finished</Text>
-      <Text style={styles.other}>
-        Category:
-        <Text style={styles.result}> computers</Text>
-      </Text>
-      <Text style={styles.other}>
-        Questions:
-        <Text style={styles.result}> 20</Text>
-      </Text>
-      <Text style={styles.other}>
-        Correct Answers:
-        <Text style={styles.result}> 15</Text>
-      </Text>
-      <Text style={styles.other}>
-        Time:
-        <Text style={styles.result}> 1.20.50</Text>
-      </Text>
+      <Animatable.View animation="zoomIn">
+        <Text style={styles.title}>Quiz Finished</Text>
+        <Text style={styles.other}>
+          Category:
+          <Text style={styles.result}> computers</Text>
+        </Text>
+        <Text style={styles.other}>
+          Questions:
+          <Text style={styles.result}> 20</Text>
+        </Text>
+        <Text style={styles.other}>
+          Correct Answers:
+          <Text style={styles.result}> 15</Text>
+        </Text>
+        <Text style={styles.other}>
+          Time:
+          <Text style={styles.result}> 1.20.50</Text>
+        </Text>
+      </Animatable.View>
     </View>
   );
 };
