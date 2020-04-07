a3b649efded14fddf07d6b228971a73082272d9a
Behzad-Rabiei <behzad.rabiei.77@gmail.com>
2020-04-07 10:31:50 +0430

    Class for Alert component created

--- App/Components/Alert/Alert.js
+++ App/Components/Alert/Alert.js
@@ -1,20 +1,23 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {View, Image} from 'react-native';
 import {setCircleStyle} from '../../util/handleStyles';
 import styles from './styles';
 
-const Alert = ({answerd, answerCorrect}) => {
-  if (!answerd) {
-    return null;
-  }
-  const {circleStyle, icon} = setCircleStyle(answerCorrect, styles);
-  return (
-    <View style={styles.container}>
-      <View style={circleStyle}>
-        <Image style={styles.icon} source={icon} resizeMode="contain" />
+class Alert extends Component {
+  render() {
+    const {answerd, answerCorrect} = this.props;
+    if (!answerd) {
+      return null;
+    }
+    const {circleStyle, icon} = setCircleStyle(answerCorrect, styles);
+    return (
+      <View style={styles.container}>
+        <View style={circleStyle}>
+          <Image style={styles.icon} source={icon} resizeMode="contain" />
+        </View>
       </View>
-    </View>
-  );
-};
+    );
+  }
+}
 
 export default Alert;
